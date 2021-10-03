8f34eaa0ca97924208783406c1943ebc7495fb1b
baizeyv <baizeyv@Gmail.com>
2021-10-03 16:55:06 +0800

    remove layout-at-startup patch

--- config.h
+++ config.h
@@ -122,7 +122,6 @@ static const XPoint stickyicon[]    = { {0,0}, {4,0}, {4,8}, {2,6}, {0,8}, {0,0}
 static const XPoint stickyiconbb    = {4,8};	/* defines the bottom right corner of the polygon's bounding box (speeds up scaling) */
 
 /* tagging */
-static const int taglayouts[] = {1, 0, 2, 3, 4, 6, 5, 7, 8};
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const char *tagsalt[] = { "a", "b", "c", "d", "e", "6", "7", "8", "9" };
 static const int momentaryalttags = 0; /* 1 means alttags will show only when key is held down*/
